f163057f608ac1e29609424155c6b421aaeeb65d
joelnarhi <joel.narhi@gmail.com>
2020-07-08 13:55:47 +0300

    2.8 tehty

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231244' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = (event) => {
       setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+      setNewNumber(event.target.value)
   }
 
   const newPerson = (event) => {
@@ -15,7 +19,8 @@ const App = () => {
       console.log('button.clicked', event.target)
 
       const personObject = {
-          name: newName
+          name: newName,
+          number: newNumber
       }
 
       console.log('anna true', persons.find(o => o.name === newName))
@@ -28,6 +33,7 @@ const App = () => {
       }
 
       setNewName('')
+      setNewNumber('')
     
     
   }
@@ -40,13 +46,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+            number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
           {persons.map(person =>
-            <li key={person.name}>{person.name}</li>
+            <li key={person.name}>{person.name} {person.number}</li>
           )}
       </ul>
     </div>
